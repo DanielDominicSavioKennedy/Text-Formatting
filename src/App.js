391eb1c7fd5e1276a73f39816d37ba29edac549d
Dominic Savio <85296900+DanielDominicSavioKennedy@users.noreply.github.com>
2022-09-13 16:39:10 +0530

    Dark mode & light mode implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react'
+
+
 function App(){
-return (
+  const [mode,setMode] = useState('light')
+  const toggleMode=()=>{
+    if(mode==='light'){
+        setMode("dark")
+        document.body.style.backgroundColor='#231a56'
+    }
+    else{
+        setMode("light")
+        document.body.style.backgroundColor='white'
+    }
+  }
+  return(
 <>
-<Navbar title="Standard Enterprises" />
+<Navbar title="Text Editor" mode={mode} toggleMode={toggleMode}/>
 
-<Textform name="Enter Text"/>
+<Textform name="Enter Text" mode={mode} />
 
 
+<About/>
 </>
 );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -8,22 +8,41 @@ export default function TextForm(props){
         let newtext=text.toUpperCase();
         setText(newtext);
         }
+        const handleLoClick = ()=>{
+            console.log("Lowercase was clicked");
+            let newtext=text.toLowerCase();
+            setText(newtext);
+            }
     const handleOnChange =(event)=>{ 
         console.log("On change");
         setText(event.target.value);
         }
-
-    const [text, setText] = useState("Enter Text Here");
+    const clear=()=>{setText("")}   
+    const [text, setText] = useState("");
    
-   return(
-<div className="container"> 
-    <div className="mb-3">
+   return( 
+    <>
+   <div className="container"> 
+    <div className="mb-5 my-4">
   
-    <h1><label for="myBox" className="form-label" >{props.name}</label></h1>    
-    <textarea className="form-control" id="myboxt" onChange={handleOnChange} value={text} rows="8" ></textarea>
-    <button type="button" onClick={handleUpClick} className="btn btn-primary">UppreCase</button>
+    <h1><label htmlFor="myBox" style={{color : props.mode==='light'?'black':'white'}} className="form-label" >{props.name}</label></h1>    
+    <textarea className="form-control" id="myboxt" onChange={handleOnChange}  style={{backgroundColor : props.mode==='light'?'white':'grey',}} value={text} rows="8" ></textarea>
+    <button type="button" onClick={handleUpClick} className="btn btn-primary my-2 mx-2">UppreCase</button>
+    <button type="button" onClick={handleLoClick} className="btn btn-primary my-2 mx-2">LowerCase</button>
+    <button type="button" onClick={clear} className="btn btn-primary my-2 mx-2">Clear</button>
+    
+    
     </div>
     </div>
-  
+    <div className="container my-1 " style={{color : props.mode==='light'?'black':'white'}}>
+        <h2>Text Summaary</h2>
+        <div>Character count: {text.length} <br/>
+              Word count: {text.length>0?text.split(" ").length:0} <br />
+              Average time to read : {text.length>0?0.008 * text.split(" ").length:0}
+              </div>
+         <h2 className='my-3'>Preview</h2>
+         <div>{text.length>0?text:"Enter text to preview"}</div>     
+    </div>
+    </>
     )
     }
